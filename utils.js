15ec7606b57f3312cabd34bb6396a3e5e727f4f3
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-07 15:27:18 +0300

    Add getOrderByTd function in utils.js

--- utils.js
+++ utils.js
@@ -10,5 +10,12 @@ const isEven = (num) => (num % 2 === 0 ? true : false);
 // Arrays
 const ANIMALS = ['cat', 'dog', 'monkey'];
 
+// Objects
+const getOrderByTd = (id) => {
+  if (!id) {
+    throw new Error('id is not defined');
+  }
+  return { id: 1, price: 10, data: '09/07/2026' };
+};
 
-module.exports = { sum, greeting, isEven, ANIMALS };
+module.exports = { sum, greeting, isEven, ANIMALS, getOrderByTd };
