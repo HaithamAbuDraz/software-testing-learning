495e62b3837e745eba34b49f7846a279b0e81a2f
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-10 10:49:30 +0300

    Add updateOrder function in db.js

--- db.js
+++ db.js
@@ -2,6 +2,11 @@ const getOrder = (orderId) => {
   return { id: orderId, price: 100 };
 };
 
+const updateOrder = (order) => {
+  console.log('order is updated !');
+};
+
 module.exports = {
   getOrder,
+  updateOrder,
 };
