92460e7ab8ae95f8b17f3dd46d19714010df83bf
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-12 12:12:13 +0300

    Add database cleanup hooks and book assertions in books.test.js

--- test/integration/books.test.js
+++ test/integration/books.test.js
@@ -2,6 +2,14 @@ require('../../config/config');
 const booksService = require('../../src/api/resources/books/books.service');
 const Book = require('../../src/api/resources/books/books.model');
 
+beforeEach(async () => {
+  await Book.deleteMany({});
+});
+
+afterAll(async () => {
+  await Book.deleteMany({});
+});
+
 describe('getBooks', () => {
   it('should return empty array', async () => {
     const books = await booksService.getBooks();
@@ -10,9 +18,11 @@ describe('getBooks', () => {
 
   it('should return 2 books', async () => {
     // insert data to db
-    await Book.insertMany([{ title: 'Book1' }, { title: 'Book2 ' }]);
+    await Book.insertMany([{ title: 'Book1' }, { title: 'Book2' }]);
 
     const books = await booksService.getBooks();
     expect(books.length).toBe(2);
+    expect(books[0]).toMatchObject({ title: 'Book1' });
+    expect(books[1]).toMatchObject({ title: 'Book2' });
   });
 });
