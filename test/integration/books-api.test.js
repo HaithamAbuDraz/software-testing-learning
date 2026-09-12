b468de4806f0fa5b4baddd6510e1ed80f368d670
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-12 17:50:54 +0300

    Add API integration tests for getBook in books-api.test.js

---
+++ test/integration/books-api.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const mongoose = require('../../config/config');
+const server = require('../../app');
+const Book = require('../../src/api/resources/books/books.model');
+
+afterEach(async () => {
+  await Book.deleteMany({});
+});
+
+afterAll(async () => {
+  server.close();
+  await mongoose.disconnect();
+});
+
+describe('getBook', () => {
+  it('should return 200 and get book from db', async () => {
+    const book = await Book.create({ title: 'MyBook' });
+    const res = await request(server).get(`/api/books/${book.id}`);
+    expect(res.status).toBe(200);
+    expect(res.body.data.book).toMatchObject({ title: 'MyBook' });
+  });
+
+  it('should return 404 if book not found', async () => {
+    const res = await request(server).get(
+      '/api/books/62b10f0171a491a27a1214e6',
+    );
+    expect(res.status).toBe(404);
+    expect(res.body.message).toMatch('not found');
+  });
+});
