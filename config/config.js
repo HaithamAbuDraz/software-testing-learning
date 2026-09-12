3dc3ffbbdcb4d06aade1785c113b3715601cf859
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-12 17:50:08 +0300

    Export mongoose instance from config/config.js

--- config/config.js
+++ config/config.js
@@ -8,16 +8,18 @@ if (env === 'testing' || env === 'test') {
   Object.assign(process.env, {
     PORT: 5000,
     DB_NAME: 'books-test',
-    MONGODB_URI: 'mongodb://user:password@localhost:27017/books-test'
+    MONGODB_URI: 'mongodb://user:password@localhost:27017/books-test',
   });
 }
 
 // database connection
 mongoose
   .connect(process.env.MONGODB_URI, {
-    authSource: 'admin'
+    authSource: 'admin',
   })
   .then(() =>
-    console.log(`connected successfully to DB: ${process.env.DB_NAME}`)
+    console.log(`connected successfully to DB: ${process.env.DB_NAME}`),
   )
   .catch((error) => console.log(`failed to connect to DB: ${error}`));
+
+module.exports = mongoose;
