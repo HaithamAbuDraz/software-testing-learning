ebf14df3a58b7c6176ff9f4fa1e4029b27d1b9c9
HaithamAbuDraz <haithamabudraz@gmail.com>
2026-09-15 16:11:35 +0300

    Add auth router and mount it in API routes

--- src/api/api-routes.js
+++ src/api/api-routes.js
@@ -3,11 +3,14 @@ const express = require('express');
 
 // own libraries
 const booksRouter = require('./resources/books/books.router');
+const authRouter = require('./resources/auth/auth.router');
 
 // create the rest router
 const restRouter = express.Router();
 
 // map the api routes
 restRouter.use('/books', booksRouter);
+restRouter.use('/auth', authRouter);
+
 
 module.exports = restRouter;
